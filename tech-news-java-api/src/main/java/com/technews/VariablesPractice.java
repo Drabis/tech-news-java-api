e1d59c9a3b51e1142e89195af7e201eeb5cad121
Aboulaye Drabo <drabis47@gmail.com>
2021-09-11 23:25:42 +0000

    created models

---
+++ tech-news-java-api/src/main/java/com/technews/VariablesPractice.java
@@ -0,0 +1,17 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args) {
+        int a = 12;
+        double b = 15.5;
+        String c = "dog";
+
+// calculate variable sum
+        double sum = a + b;
+// calculated variable string and int
+        String concat = a + c;
+
+        System.out.println(sum);
+        System.out.println(concat);
+    }
+}
